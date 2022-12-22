04f2304ffcd662ea6f7af6f8325da327c79d58d9
youssefelrefa3i <84276712+youssefelrefa3i@users.noreply.github.com>
2022-12-22 20:35:49 +0200

    Add files via upload

---
+++ Insertion Sort C#.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace ins
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int[] arr = new int[10];
+            int i;
+            Console.Write("Input 10 elements in the array :\n");
+            for (i = 0; i < 10; i++)
+            {
+                Console.Write("element {0} : ", i);
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.Write("\nElements in array are: ");
+            for (i = 0; i < 10; i++)
+            {
+                Console.Write("{0}  ", arr[i]);
+            }
+            Console.Write("\n");
+            int n = 10, j, val, flag;
+            Console.WriteLine("\n"+"Insertion Sort");
+            Console.Write("\n"+"Initial array is: ");
+            for (i = 0; i < n; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+            for (i = 1; i < n; i++)
+            {
+                val = arr[i];
+                flag = 0;
+                for (j = i - 1; j >= 0 && flag != 1;)
+                {
+                    if (val < arr[j])
+                    {
+                        arr[j + 1] = arr[j];
+                        j--;
+                        arr[j + 1] = val;
+                    }
+                    else flag = 1;
+                }
+            }
+
+            Console.Write("\n"+"Sorted Array is: ");
+            for (i = 0; i < n; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+
+        }
+        public int[] SortArray(int[] array, int length)
+        {
+            for (int i = 1; i < length; i++)
+            {
+                var key = array[i];
+                var flag = 0;
+
+                for (int j = i - 1; j >= 0 && flag != 1;)
+                {
+                    if (key < array[j])
+                    {
+                        array[j + 1] = array[j];
+                        j--;
+                        array[j + 1] = key;
+                    }
+                    else flag = 1;
+                }
+            }
+
+            return array;
+        }
+    }
+}
